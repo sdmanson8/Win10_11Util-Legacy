c5d65e86cc4e76a379e1f4798ddcc049b36d1aa4
sdmanson8 <sdmanson@yahoo.com>
2026-03-21 17:40:57 +0200

    final changes

--- Script_Files/Main Script.ps1
+++
@@ -1,3 +0,0 @@
-# Main Script Download in order to use Combined Scripts
-    $ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Script.ps1" -UseBasicParsing
-    Invoke-Expression $($ScriptFromGithHub.Content)
--- Script_Files/PatchMyPC.ps1
+++
@@ -1,3 +0,0 @@
-$downloads=(New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
-Invoke-WebRequest -URI "https://homeupdater.patchmypc.com/public/PatchMyPC-HomeUpdater.msi?_gl=1*1gxmdjs*_gcl_au*MTg3OTQ2MTQ2Ny4xNzM1MDM1MjQ5" -OutFile $downloads\PatchMyPC.msi -UseBasicParsing
-& $downloads\PatchMyPC.msi
